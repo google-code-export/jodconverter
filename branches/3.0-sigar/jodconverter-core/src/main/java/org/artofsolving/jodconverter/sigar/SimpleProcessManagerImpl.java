0d32df2d43abc2641384d237b37f71524e0199b8
shervin.asgari@gmail.com <shervin.asgari@gmail.com@e8ee3d98-163c-0410-9f04-db2354e518cf>
2011-03-17 14:27:55 +0000

    - Added Google Guava (it simply rocks)

--- branches/3.0-sigar/jodconverter-core/src/main/java/org/artofsolving/jodconverter/sigar/SimpleProcessManagerImpl.java
+++ branches/3.0-sigar/jodconverter-core/src/main/java/org/artofsolving/jodconverter/sigar/SimpleProcessManagerImpl.java
@@ -1,12 +1,12 @@
 package org.artofsolving.jodconverter.sigar;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 import org.hyperic.sigar.ptql.ProcessFinder;
+
+import com.google.common.primitives.Longs;
 
 /**
  * @author Shervin Asgari - <a href="mailto:shervin.asgari@redpill-linpro.com">shervin@redpill-linpro.com</a>
@@ -18,38 +18,29 @@ public class SimpleProcessManagerImpl implements SimpleProcessManager {
 	 * library loaded within the process Returns a Set of all the process id's that was found
 	 * 
 	 * @param query
-	 * @param searchValue
-	 *            - The value you want to search for
-	 * @return - Returns immutable Set of process id's as long or an empty Set if no results where found
+	 * @param searchValue - The value you want to search for
+	 * @return - Returns immutable List of process id's as Long or an empty List if no results where found
 	 * @see org.artofsolving.jodconverter.sigar.SimpleProcessManager#find(SimplePTQL)
 	 */
-	public List<Long> find(SimplePTQL ptlq) throws SigarException {
+	public List<Long> find(SimplePTQL ptql) throws SigarException {
 		Sigar sigar = new Sigar();
 		try {
 			ProcessFinder processFinder = new ProcessFinder(sigar);
-			long[] find = processFinder.find(ptlq.getQuery());
-			if (find.length > 0) {
-				List<Long> list = new ArrayList<Long>(find.length);
-				for (long value : find) {
-					list.add(value);
-				}
-				return list;
-			}
-
+			long[] find = processFinder.find(ptql.getQuery());
+			return Longs.asList(find);
 		} finally {
 			sigar.close();
 		}
-		return Collections.emptyList();
 	}
 
 	/**
 	 * @see org.artofsolving.jodconverter.sigar.SimpleProcessManager#findSingle(org.artofsolving.jodconverter.sigar.SimplePTQL)
 	 */
-	public Long findSingle(SimplePTQL ptlq) throws NonUniqueResultException, SigarException {
+	public Long findSingle(SimplePTQL ptql) throws NonUniqueResultException, SigarException {
 		Sigar sigar = new Sigar();
 		try {
 			ProcessFinder processFinder = new ProcessFinder(sigar);
-			long[] find = processFinder.find(ptlq.getQuery());
+			long[] find = processFinder.find(ptql.getQuery());
 			if (find.length == 1) {
 				return Long.valueOf(find[0]);
 			} else if (find.length > 1) {
@@ -59,7 +50,7 @@ public class SimpleProcessManagerImpl implements SimpleProcessManager {
 			sigar.close();
 		}
 
-		return Long.valueOf(0L);
+		return 0L;
 	}
 
 	public void kill(long pid, int signium) throws SigarException {
@@ -70,5 +61,4 @@ public class SimpleProcessManagerImpl implements SimpleProcessManager {
 			sigar.close();
 		}
 	}
-
 }
--- branches/3.0-sigar/jodconverter-core/src/test/java/org/artofsolving/jodconverter/office/PooledOfficeManagerTest.java
+++ branches/3.0-sigar/jodconverter-core/src/test/java/org/artofsolving/jodconverter/office/PooledOfficeManagerTest.java
@@ -25,10 +25,14 @@ import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
+import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.TimeoutException;
 
 import org.artofsolving.jodconverter.ReflectionUtils;
+import org.artofsolving.jodconverter.sigar.SimplePTQL;
+import org.artofsolving.jodconverter.sigar.SimpleProcessManagerImpl;
+import org.artofsolving.jodconverter.sigar.SimplePTQL.Strategy;
 import org.artofsolving.jodconverter.util.PlatformUtils;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.ptql.ProcessFinder;
@@ -173,6 +177,42 @@ public class PooledOfficeManagerTest {
         assertEquals(process.getExitCode(0, 0), 0);
     }
     
+    public void checkEntireArgument() throws Exception {
+    	PooledOfficeManager officeManager = new PooledOfficeManager(CONNECTION_MODE);
+        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager, "managedOfficeProcess");
+        OfficeProcess process = (OfficeProcess) ReflectionUtils.getPrivateField(managedOfficeProcess, "process");
+        OfficeConnection connection = (OfficeConnection) ReflectionUtils.getPrivateField(managedOfficeProcess, "connection");
+        officeManager.start();
+        assertTrue(process.isRunning());
+        assertTrue(connection.isConnected());
+        
+        UnoUrl unoUrl = (UnoUrl) ReflectionUtils.getPrivateField(process, "unoUrl");
+        
+		SimplePTQL ptql = new SimplePTQL.Builder(SimplePTQL.STATE_NAME(), SimplePTQL.RE(), "soffice.*")
+							.addArgs(1, SimplePTQL.RE(), "-accept=" + unoUrl.getAcceptString() + ";urp;", Strategy.ESCAPE)
+							.addArgs(2, SimplePTQL.RE(), "-env:UserInstallation=.+", Strategy.ESCAPE)
+							.addArgs(3, SimplePTQL.EQ(), "-headless", Strategy.NOT_ESCAPE)
+							.addArgs(4, SimplePTQL.EQ(), "-nocrashreport", Strategy.NOT_ESCAPE)
+							.addArgs(5, SimplePTQL.EQ(), "-nodefault", Strategy.NOT_ESCAPE)
+							.addArgs(6, SimplePTQL.EQ(), "-nofirststartwizard", Strategy.NOT_ESCAPE)
+							.addArgs(7, SimplePTQL.EQ(), "-nolockcheck", Strategy.NOT_ESCAPE)
+							.addArgs(8, SimplePTQL.EQ(), "-nologo", Strategy.NOT_ESCAPE)
+							.addArgs(9, SimplePTQL.EQ(), "-norestore", Strategy.NOT_ESCAPE)
+							.createQuery();
+		
+		List<Long> find = new SimpleProcessManagerImpl().find(ptql);
+		Assert.assertTrue(find.size() == 1);
+		
+		MockOfficeTask task = new MockOfficeTask();
+        officeManager.execute(task);
+        assertTrue(task.isCompleted());
+        
+        officeManager.stop();
+        assertFalse(connection.isConnected());
+        assertFalse(process.isRunning());
+        assertEquals(process.getExitCode(0, 0), 0);
+    }
+    
     public void checkAcceptString() throws Exception {
     	PooledOfficeManager officeManager = new PooledOfficeManager(CONNECTION_MODE);
         ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager, "managedOfficeProcess");
@@ -190,7 +230,6 @@ public class PooledOfficeManagerTest {
 		//Why doesn't this acceptstring contain \Q and \E but when running live does?
 		long[] pids = processFinder.find(PTQL + PlatformUtils.escapePTQLForRegex(unoUrl.getAcceptString()));
 		Assert.assertTrue(pids.length == 1);
-        
         MockOfficeTask task = new MockOfficeTask();
         officeManager.execute(task);
         assertTrue(task.isCompleted());
